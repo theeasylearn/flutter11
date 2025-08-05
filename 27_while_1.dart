d6029ce7481a11c9314247a02665e9d0300424d5
theeasylearn <theeasylearn@gmail.com>
2025-08-05 11:32:30 +0530

    exercise

---
+++ 27_while_1.dart
@@ -0,0 +1,12 @@
+//1, 3, 5, 7, 9, 11 .... 100
+import "dart:io";
+void main()
+{
+    int num = 1;
+    while(num<=100) // > < >= <= == !=
+    {
+        stdout.write("$num ");
+        num = num + 2; //3
+    }
+    
+}
