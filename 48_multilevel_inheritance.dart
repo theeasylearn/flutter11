0d70f2079219b8835e2ca967e1b789ac365fe6d5
theeasylearn <theeasylearn@gmail.com>
2025-09-04 11:33:52 +0530

    inheritance

--- 48_multilevel_inheritance.dart
+++ 48_multilevel_inheritance.dart
@@ -1,5 +1,65 @@
 // example of multilevel inheritance
+import 'dart:io';
 class KB
 {
-    
+    //instance variable
+    int bytes = 0;
+    KB(int bytes)
+    {
+        this.bytes = bytes;
+        print("KB class constructor called...");
+    }
+    double getKB()
+    {
+        //return kilobytes 
+        double temp = this.bytes / 1024.0;
+        return temp;
+    }
+}
+//single level inheritance
+//derived class
+class MB extends KB
+{
+    MB(int bytes) : super(bytes)
+    {
+        //calling parent class constructor(required)
+        print("MB class constructor executed...");
+    }
+    double getMB()
+    {
+        double kiloBytes = super.getKB(); //return kilobytes 
+        double megaBytes = kiloBytes / 1024.0; 
+        return megaBytes;
+    }
+}
+class GB extends MB 
+{
+    GB(int bytes) : super(bytes)
+    {
+        print("GB class constructor executed...");
+    }
+    double getGB()
+    {
+        double gigaBytes = super.getMB() / 1024.0;
+        return gigaBytes;
+    }
+}
+void main()
+{
+    int bytes;
+    print("Enter bytes");
+    bytes = int.parse(stdin.readLineSync().toString());
+    // KB k1 = new KB(bytes);
+    // double kiloBytes = k1.getKB();
+    // print("Kilo Bytes = $kiloBytes");
+
+    // MB m1 = new MB(bytes);
+    GB g1 = new GB(bytes);
+    double megaBytes = g1.getMB();
+    double kiloBytes = g1.getKB();
+    double gigaBytes = g1.getGB();
+    print("Kilo Bytes = $kiloBytes");
+    print("Mega Bytes = $megaBytes");
+    print("Giga Bytes = $gigaBytes");
+
 }
--- 50_hiearchical_inheritance.dart
+++ 50_hiearchical_inheritance.dart
@@ -14,7 +14,7 @@ class MyMath
     }
     
 }
-//child class 
+//1st child class 
 class Circle extends MyMath
 {
     //instance variable 
@@ -30,14 +30,43 @@ class Circle extends MyMath
         return area;
     }
 }
+//2nd child class 
+class Cyliender extends MyMath
+{
+    //instance variable
+    int radius=0,height=0;
+    Cyliender(int radius,int height)
+    {
+        this.radius = radius;
+        this.height = height;
+    }
+    double getVolume()
+    {
+        double volume = super.getPi() * super.getSquare(this.radius) * this.height;
+        return volume;
+    }
+}
 void main()
 {
-    int radius;
+    int radius,height;
     print("Enter circle's radius");
     radius = int.parse(stdin.readLineSync().toString());
+
 
     //create circle class object
     Circle c1 = new Circle(radius);
     double area = c1.getArea();
     print("Area = $area");
+
+
+    print("Enter cyliender's radius");
+    radius = int.parse(stdin.readLineSync().toString());
+
+    print("Enter cyliender's height");
+    height = int.parse(stdin.readLineSync().toString());
+
+
+    Cyliender c2 = new Cyliender(radius,height); //calling constructor
+    double volume = c2.getVolume();
+    print("volume = $volume");
 }
